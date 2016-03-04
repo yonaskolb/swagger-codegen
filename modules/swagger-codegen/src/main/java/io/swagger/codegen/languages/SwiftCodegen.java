58acb7939d5bc4e8c4251ded3c61b788491dd95f
Yonas Kolb <yonas.kolb@massiveinteractive.com>
2016-03-04 16:56:49 +1100

    renamed escaped enum from SwaggerType

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SwiftCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SwiftCodegen.java
@@ -98,7 +98,7 @@ public class SwiftCodegen extends DefaultCodegen implements CodegenConfig {
         "true", "lazy", "operator", "in", "COLUMN", "left", "private", "return", "FILE", "mutating", "protocol",
         "switch", "FUNCTION", "none", "public", "where", "LINE", "nonmutating", "static", "while", "optional",
         "struct", "override", "subscript", "postfix", "typealias", "precedence", "var", "prefix", "Protocol",
-        "required", "right", "set", "Type", "unowned", "weak")
+        "required", "right", "set", "unowned", "weak", "Type")
     );
 
     typeMapping = new HashMap<String, String>();
@@ -266,7 +266,7 @@ public class SwiftCodegen extends DefaultCodegen implements CodegenConfig {
       codegenProperty.datatypeWithEnum =
               StringUtils.left(codegenProperty.datatypeWithEnum, codegenProperty.datatypeWithEnum.length() - "Enum".length());
       if (reservedWords.contains(codegenProperty.datatypeWithEnum)) {
-        codegenProperty.datatypeWithEnum = escapeReservedWord(codegenProperty.datatypeWithEnum);
+        codegenProperty.datatypeWithEnum = codegenProperty.datatypeWithEnum + "Enum";
       }
     }
     return codegenProperty;
